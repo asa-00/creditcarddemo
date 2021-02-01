023b5d87d7aadcc898fe87e8dc2898f8ed0acd80
Ali Ahmed <aliahmed.skv@gmail.com>
2021-02-01 19:49:06 +0100

    Card back elements in place

--- src/components/card/CreditCard.jsx
+++ src/components/card/CreditCard.jsx
@@ -18,24 +18,29 @@ const CreditCard = (props) => {
                     <img className="type" src={visa} alt="Card Type" />
                 </div>
                 <div className="credit-card-number">
-                    <div className="credit-card-number-label" for="cardExpiration">{intialValues.cardNumber}</div>
+                    <div className="credit-card-number-label" htmlFor="cardExpiration">{intialValues.cardNumber}</div>
                 </div>
                 <div className='card-holder-wrapper'></div>
                 <div className="card-holder-expiration">
                     <span className="card-holder">
-                        <label className="card-holder-label" for="cardHolderLabel">Card Holder</label>
-                        <label className="card-holder-name" for="cardHolderName">{intialValues.cardHolderName}</label>
+                        <label className="card-holder-label" htmlFor="cardHolderLabel">Card Holder</label>
+                        <label className="card-holder-name" htmlFor="cardHolderName">{intialValues.cardHolderName}</label>
                     </span>
                     <span className="card-expiration">
-                        <label className="card-expiration-label" for="cardExpiration">Expires</label>
-                        <label className="card-expiration-date" for="cardExpiration">{intialValues.expiration}</label>
+                        <label className="card-expiration-label" htmlFor="cardExpiration">Expires</label>
+                        <label className="card-expiration-date" htmlFor="cardExpiration">{intialValues.expiration}</label>
                     </span>
                 </div>
                 </div>
-                <div class="card-back">
-                    <h1>Form</h1>
-                    <p></p>
-                    <p></p>
+                <div className="card-back">
+                    <div className='card-magnet'></div>
+                    <div className='cvv-container'>
+                        <label className="card-cvv-label" htmlFor="cardCvvLabel">CVV</label>
+                        <div className='card-cvv'><span className='cvv'>***</span></div>
+                    </div>
+                    <div className="typ-img-container">  
+                        <img className="card-back-type-img" src={visa} alt="Card Type" />
+                    </div>
                 </div>
             </div>
         </div>
